f1c4abcf6e72e46fc86ec61ae9e922a7a6451a8f
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2022-06-11 22:39:50 -0300

    'backend'

--- src/middlewares/isAuthenticated.ts
+++ src/middlewares/isAuthenticated.ts
@@ -12,5 +12,29 @@ export function isAuthenticated(
 ){
 
     // Receber o Token
+    const authToken = req.headers.authorization;
+
+    if(!authToken){
+        return res.status(401).end();
+    }
+
+    const [, token] = authToken.split(" ")
+
+    try{
+        //Validar esse token
+
+        const {sub} = verify(
+            token,
+            process.env.JWT_SECRET
+        ) as PayLoad;
+        
+        //Recuperar o id do token e colocar dentro de uma variavel user_id dentro do req
+        req.user_id = sub;
+
+        return next();
+
+    }catch(err){
+        return res.status(401).end();
+    }
 
 }
